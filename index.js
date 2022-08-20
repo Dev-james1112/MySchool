fda3c289ceec28d22f40ba9fb7debeffaf95aa43
james1112 <jiwon1112kim@naver.com>
2022-08-20 23:23:30 +0900

    [CHORE] ver 0.5.0
    react-native cli -> expo cli

--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+import 'expo-asset';
 AppRegistry.registerComponent(appName, () => App);
